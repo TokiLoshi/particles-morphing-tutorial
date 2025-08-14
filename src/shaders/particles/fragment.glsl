cf9e71701f713598a68a083230f1815a68c96f05
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2025-08-13 18:33:29 -0700

    particles morphing tutorial

---
+++ src/shaders/particles/fragment.glsl
@@ -0,0 +1,10 @@
+void main()
+{   vec2 uv = gl_PointCoord; 
+    // Distance from the center 
+    float distanceToCenter = length(uv - 0.5);
+    // small number division technique
+    float alpha = 0.05 / distanceToCenter - 0.1;
+    gl_FragColor = vec4(1.0, 1.0, 1.0, alpha);
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
