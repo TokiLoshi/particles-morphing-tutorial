aa31113b49275af0977dd5c6f8572c9a0a81ff42
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2025-08-27 19:50:44 -0700

    adding morphing animations to lil gui

--- src/script.js
+++ src/script.js
@@ -95,12 +95,11 @@ renderer.setClearColor(debugObject.clearColor);
 // Load models
 let particles = null;
 gltfLoader.load("./models.glb", (gltf) => {
-	console.log(gltf);
-
 	/**
 	 * Particles
 	 */
 	particles = {};
+	particles.index = 0;
 
 	// Positions
 	const positions = gltf.scene.children.map(
@@ -139,8 +138,14 @@ gltfLoader.load("./models.glb", (gltf) => {
 
 	// Geometry
 	particles.geometry = new THREE.BufferGeometry();
-	particles.geometry.setAttribute("position", particles.positions[1]);
-	particles.geometry.setAttribute("aPositionTarget", particles.positions[3]);
+	particles.geometry.setAttribute(
+		"position",
+		particles.positions[particles.index]
+	);
+	particles.geometry.setAttribute(
+		"aPositionTarget",
+		particles.positions[particles.index]
+	);
 	// set index to null to reduce the number of vertices and make the sphere less bright
 	// particles.geometry.setIndex(null);
 
@@ -166,6 +171,36 @@ gltfLoader.load("./models.glb", (gltf) => {
 	particles.points = new THREE.Points(particles.geometry, particles.material);
 	scene.add(particles.points);
 
+	particles.morph = (index) => {
+		// Update attributes
+		particles.geometry.attributes.position =
+			particles.positions[particles.index];
+
+		particles.geometry.attributes.aPositionTarget = particles.positions[index];
+
+		// Animate uProgress
+		gsap.fromTo(
+			particles.material.uniforms.uProgress,
+			{ value: 0 },
+			{ value: 1, duration: 3, ease: "linear" }
+		);
+
+		// Save index
+		particles.index = index;
+	};
+
+	particles.morph0 = () => {
+		particles.morph(0);
+	};
+	particles.morph1 = () => {
+		particles.morph(1);
+	};
+	particles.morph2 = () => {
+		particles.morph(2);
+	};
+	particles.morph3 = () => {
+		particles.morph(3);
+	};
 	// Tweaks
 	gui
 		.add(particles.material.uniforms.uProgress, "value")
@@ -173,6 +208,10 @@ gltfLoader.load("./models.glb", (gltf) => {
 		.max(1)
 		.step(0.001)
 		.name("uProgress");
+	gui.add(particles, "morph0");
+	gui.add(particles, "morph1");
+	gui.add(particles, "morph2");
+	gui.add(particles, "morph3");
 });
 
 /**
